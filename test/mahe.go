11240b3a64611ac5d8dae35f54201af54ea65061
Mahesh <jesuscyrilpaul@gmail.com>
2024-03-10 14:01:27 +0530

    clones & added

---
+++ test/mahe.go
@@ -0,0 +1,11 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, mahe") // Hello, mahe
+	fmt.Println("Hello world") // Hello world
+	fmt.Println(1 + 5)         // 6
+}
